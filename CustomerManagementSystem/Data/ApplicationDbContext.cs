34efdff2ef8f568c62c32ecaca777e5a3175231a
Toprak <toprak.kamburoglu@stu.khas.edu.tr>
2024-09-29 18:07:58 +0300

    Customer Management System
    This system allows business owners to regulate their customer informations at their databases.

---
+++ CustomerManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CustomerManagementSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerManagementSystem.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //Tabloları Ekleme
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ CustomerManagementSystem/Models/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace CustomerManagementSystem.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+   
+        public required string Name { get; set; }
+
+        
+        public required string Email { get; set; }
+
+        [DisplayName("Adress")]
+        public string Address { get; set; } = string.Empty;
+
+        
+        public string City { get; set; } = string.Empty;
+
+       
+        public string Region { get; set; } = string.Empty;
+
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; } = string.Empty;
+
+       
+        public string Country { get; set; } = string.Empty;
+
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        
+        public string Fax { get; set; } = string.Empty;
+
+        [DisplayName("VAT Number")]
+        public string VATNumber { get; set; } = string.Empty;
+
+    }
+}
